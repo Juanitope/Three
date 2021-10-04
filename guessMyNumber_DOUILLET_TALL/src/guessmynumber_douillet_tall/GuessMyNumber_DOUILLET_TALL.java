3517ac1999d8ee26bef214bbd6dc78c0fdb560e3
douil <douil@LAPTOP-IDFMUE7S>
2021-10-04 12:18:08 +0200

    Debut

--- guessMyNumber_DOUILLET_TALL/src/guessmynumber_douillet_tall/GuessMyNumber_DOUILLET_TALL.java
+++ guessMyNumber_DOUILLET_TALL/src/guessmynumber_douillet_tall/GuessMyNumber_DOUILLET_TALL.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package guessmynumber_douillet_tall;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +13,42 @@ public class GuessMyNumber_DOUILLET_TALL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choissisez uen difficulté : Facile,Normal,Difficile");
+        String difficulte=sc.next();
+        
+        
+        Random generateurAleat = new Random();
+        int n = generateurAleat.nextInt(100);
+        //System.out.println(n);
+        
+        
+        int nombre;
+        System.out.println("Entre un nombre entre 0 et 100");
+        nombre = sc.nextInt();
+        int essais= 1;
+        if (nombre == n) {
+            System.out.println("Vous avez trouvez le bon numéro");
+        }
+
+        if (nombre != n) {
+            while (nombre != n) {
+                
+                if (nombre > n) {
+                    System.out.println("Plus petit");
+                    nombre = sc.nextInt();
+                    essais= essais +1;
+                }
+                if (nombre < n) {
+                    System.out.println("Plus grand");
+                    nombre = sc.nextInt();
+                    essais= essais +1;
+                }
+                if (nombre == n) {
+                    System.out.println("Vous avez trouvez le bon numéro");
+                }
+            }
+        }
+        System.out.println("Vous avez mis "+essais+" essais");
     }
-    
 }
