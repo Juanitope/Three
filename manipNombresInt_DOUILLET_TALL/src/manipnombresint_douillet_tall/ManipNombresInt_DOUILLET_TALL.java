a01267c0b11047b827e93dee3a7e6c2436ad499c
douil <douil@LAPTOP-IDFMUE7S>
2021-10-04 11:14:23 +0200

    Debut projet

---
+++ manipNombresInt_DOUILLET_TALL/src/manipnombresint_douillet_tall/ManipNombresInt_DOUILLET_TALL.java
@@ -0,0 +1,28 @@
+package manipnombresint_douillet_tall;
+import java.util.Scanner;
+/**
+ *
+ * @author douil
+ */
+public class ManipNombresInt_DOUILLET_TALL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int entier1;
+      int entier2; 
+      entier1=sc.nextInt();
+      entier2=sc.nextInt();
+      System.out.println(entier1 + " et " +entier2);
+      int somme = entier1 + entier2 ; 
+      System.out.println("La somme est "+ somme);
+      int diff = entier1 - entier2 ; 
+      System.out.println("La différence est "+ diff);
+      int produit = entier1 * entier2 ; 
+      System.out.println("Le produit est "+ produit);
+      
+    }
+    
+}
