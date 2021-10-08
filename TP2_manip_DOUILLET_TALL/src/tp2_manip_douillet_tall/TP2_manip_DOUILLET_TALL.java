5415ee6b4d9f71c164768d0e39a9791183705563
douil <douil@LAPTOP-IDFMUE7S>
2021-10-08 11:19:35 +0200

    10 reference moussaka

---
+++ TP2_manip_DOUILLET_TALL/src/tp2_manip_douillet_tall/TP2_manip_DOUILLET_TALL.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_douillet_tall;
+
+/**
+ *
+ * @author douil
+ */
+public class TP2_manip_DOUILLET_TALL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ; 
+        assiette1.nbCalories += 1000;
+        System.out.println("nb de calories de Assiette 1 : " +assiette1.nbCalories) ;
+        System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories) ;
+        int inverseur = assiette1.nbCalories;
+        assiette1.nbCalories=assiette2.nbCalories;
+        assiette2.nbCalories=inverseur;
+        
+        Moussaka assiette666 = assiette1 ;
+        
+        //Moussaka assiette667 = new Tartiflette() ;
+
+    }
+    
+}
---
+++ TP2_manip_DOUILLET_TALL/src/tp2_manip_douillet_tall/Tartiflette.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_douillet_tall;
+
+/**
+ *
+ * @author douil
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+public Tartiflette(int calo) {
+    nbCalories = calo; 
+    }
+
+
+}
