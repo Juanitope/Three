eda4f2391f64c543e5435651b94f70cc2bf040fc
douil <douil@LAPTOP-IDFMUE7S>
2021-10-18 17:08:21 +0200

    Q4

---
+++ TP2_relation_1_DOUILLET_TALL/src/tp2_relation_1_douillet_tall/Personne.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_douillet_tall;
+
+/**
+ *
+ * @author douil
+ */
+public class Personne {
+    String nom;
+    String prenom;
+    
+    public Personne(String Unnom, String Unprenom){
+        nom=Unnom;
+        prenom=Unprenom;
+        
+    }
+    @Override
+    public String toString() { 
+        }
+}
---
+++ TP2_relation_1_DOUILLET_TALL/src/tp2_relation_1_douillet_tall/Voiture.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_douillet_tall;
+
+/**
+ *
+ * @author douil
+ */
+public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+    
+ public Voiture(String LeModele, String LaMarque, int LaPuissanceCV) {
+    Modele = LeModele ;
+    Marque = LaMarque;
+    PuissanceCV = LaPuissanceCV;
+    }
+        @Override
+        public String toString() { 
+        }
+}
