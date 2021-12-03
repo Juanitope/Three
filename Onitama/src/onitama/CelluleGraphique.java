eadb90c10f6b7b01a86ef0c76acfeac6a3aa4fcb
douil <douil@LAPTOP-IDFMUE7S>
2021-12-03 11:24:21 +0100

    Start graphique

---
+++ Onitama/src/onitama/CelluleGraphique.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package onitama;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author douil
+ */
+public class CelluleGraphique extends JButton {
+    //Cellule celluleAssociee;
+    int CelluleChoisie;
+    //ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getRessource("/Pion/PionVide.png"));
+    /*public CelluleGraphique (Cellule uneCellule){
+        celluleAssociee = uneCellule;
+    }
+    
+    @Override
+    public void paintComponent (Graphics G){
+        super.paintComponent(G);
+        setIcon(PionVide);
+    }
+    */
+}
