209f3dded6907f62b386ca7fb4bb89ab4d84d840
douil <douil@LAPTOP-IDFMUE7S>
2022-01-03 21:45:37 +0100

    Finition des classes

--- Three/src/three/Grille.java
+++ Three/src/three/Grille.java
@@ -9,31 +9,41 @@ import java.util.Random;
  * @author douil
  */
 public class Grille {
-    int Grille[][]= new int[5][5];
+    int GrilledeJeu[][]= new int[5][5];
     int carte;
    
     
     
-    public void Grille(){
+    public void CréationGrille(){
         for (int i=0; i<5 ; i++){
             for (int j=0; j<5 ;j++){
-                Grille[i][j]=0;
+                GrilledeJeu[i][j]=0;
             }
         }
     }
+    
     public void NewCarte(){
         Random random = new Random();
-        carte = random.nextInt(4)+1; // à vérifier si c'est entre 1 et 3
+        carte = random.nextInt(3)+1; // à vérifier si c'est entre 1 et 3
         
     }
             
-            
-            
-       public boolean partieFini(){
+          
     
-           //Si impossible de jouer :
+    
+    
+           public boolean partieFini(){
+               
+                
+
+                
+           //Si la partie est finie (soit la fonction retourne true)
+           //Soit impossible de jouer :
+           // ----> Calcul du score final , en fonction des cartes présentes
            return true;
            
-       }
+       }         
+            
+
     
 }
